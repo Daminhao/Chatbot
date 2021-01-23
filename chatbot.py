688bc32b7b6e6c1f13f75036cdecab6b19bd2097
Danilo <54450923+Daminhao@users.noreply.github.com>
2021-01-23 17:28:59 -0300

    Chat bot com if else
    Agora entendi essa parada!

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,10 @@
-#Apresentação
-print ('olá, digite seu nome:')
-#Pergunta
-text = input ('Nome:')
+#Chat bot com else
+# 1 objeto
+a = 3
+#2 se a for igual a 2 faça 
+if a == 2:
+    print ('a = 2')
+#3 se não, faça isso
 
-print('Olá '+ text)
+else:
+    print('a não é 2')   
